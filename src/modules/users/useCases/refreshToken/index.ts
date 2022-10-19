4d9612f2d407089306eb73b1ecdc611278979b7e
Jane Luz <janerodrigues_2610@hotmail.com>
2022-10-19 18:05:23 -0300

    remove folder refreshToken

--- src/modules/users/useCases/refreshToken/index.ts
+++
@@ -1,9 +0,0 @@
-// import { UsersRepository } from "../../repositories/implementations/UsersRepository";
-// import {  UpdateTokenController } from "./refreshTokenController";
-// import {  UpdateTokenUseCase } from "./refreshTokenUseCase";
-
-// const usersRepository = UsersRepository.getInstance();
-// const updateTokenUseCase = new UpdateTokenUseCase(usersRepository);
-// const updateTokenController = new UpdateTokenController(updateTokenUseCase);
-
-// export { updateTokenController};
